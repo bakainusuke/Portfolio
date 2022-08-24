06cc3b0cf93a8ff6931b06667d462de6bd763395
bakainusuke <s3894502@student.rmit.edu.au>
2022-08-24 19:15:29 +1000

    new commit
    Signed-off-by: bakainusuke <s3894502@student.rmit.edu.au>

---
+++ src/pages/Signin.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function signin(props) {
+  return (
+    <div>
+      <h1>Sign In</h1>
+    </div>
+  );
+}
+export default signin;
