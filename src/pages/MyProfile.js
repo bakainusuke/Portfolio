ddbe2410690538f5cae62ee7aa01cb0928f86cc0
bakainusuke <s3894502@student.rmit.edu.au>
2022-08-27 20:56:26 +1000

    update edit user layout
    Signed-off-by: bakainusuke <s3894502@student.rmit.edu.au>

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -1,10 +1,14 @@
 import { fromUnixTime } from "date-fns";
 import React, { useState } from "react";
 import { getUser, removeUser, deleteUser } from "../data/repository";
-import { useNavigate } from "react-router-dom";
+
+import { Button, Modal } from "react-bootstrap";
 const USER_KEY = "user";
 
 function MyProfile(props) {
+  const [deleteShow, setDeShow] = useState(false);
+  const [editShow, setEdShow] = useState(false);
+
   let user = [];
   user = JSON.parse(localStorage.getItem(USER_KEY));
 
@@ -16,7 +20,6 @@ function MyProfile(props) {
       result.getMonth() + 1
     }/${result.getFullYear()}`;
   }
-  const navigate = useNavigate();
 
   const handleDelete = (userId) => {
     console.log("delete not done");
@@ -45,20 +48,67 @@ function MyProfile(props) {
             </div>
 
             <div className="form-group">
-              <input
-                type="submit"
-                className="btn btn-primary btn-round m-3"
-                value="Edit"
-              />
-              <input
-                type="submit"
-                className="btn btn-danger mr-5"
-                value="Delete"
-                onClick={(e) => {
-                  e.preventDefault();
-                  handleDelete(user.userId);
-                }}
-              />
+              <>
+                <input
+                  type="submit"
+                  className="btn btn-primary btn-round m-3"
+                  value="Edit"
+                  onClick={() => setEdShow(true)}
+                />
+                <Modal show={editShow} onHide={() => setEdShow(false)}>
+                  <Modal.Header closeButton>
+                    <Modal.Title>Edit User Details</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>Edit your details</Modal.Body>
+                  <Modal.Footer>
+                    <Button
+                      variant="secondary"
+                      onClick={() => setEdShow(false)}
+                    >
+                      Close
+                    </Button>
+                    <input
+                      type="submit"
+                      className="btn btn-success mr-5"
+                      value="Confirm"
+                      onClick={(e) => {}}
+                    />
+                  </Modal.Footer>
+                </Modal>
+              </>
+              <>
+                <input
+                  type="submit"
+                  className="btn btn-danger mr-5"
+                  onClick={() => setDeShow(true)}
+                  value="Delete"
+                />
+                <Modal show={deleteShow} onHide={() => setDeShow(false)}>
+                  <Modal.Header closeButton>
+                    <Modal.Title>Delete User</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>
+                    This will permanently delete your Account.
+                  </Modal.Body>
+                  <Modal.Footer>
+                    <Button
+                      variant="secondary"
+                      onClick={() => setDeShow(false)}
+                    >
+                      Close
+                    </Button>
+                    <input
+                      type="submit"
+                      className="btn btn-danger mr-5"
+                      value="Delete"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        handleDelete(user.userId);
+                      }}
+                    />
+                  </Modal.Footer>
+                </Modal>
+              </>
             </div>
           </div>
         </div>
